11a03bd332648fe79aa181e9b0f0d163e8b692b1
Jaime J Cobo <jaimecobo@outlook.com>
2021-08-23 13:50:42 -0600

    Full CRUD Blog_Application

---
+++ Week_10/Day_1_BlogAppCompleted/src/main/java/com/tts/blog_app/Blog_Application.java
@@ -0,0 +1,13 @@
+package com.tts.blog_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blog_Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blog_Application.class, args);
+	}
+
+}
