f648d17e650f430daccff9eb2fb1df7fca2aad7b
Jaime J Cobo <jaimecobo@outlook.com>
2021-08-25 17:30:23 -0600

    Address Book Spring Boot Project - repository directory + AddressBookRepository Interface

--- Week_10/Day_2_AddressBook/addressBook/src/main/java/com/jc/addressBook/repository/AddressBookRepository.java
+++ Week_10/Day_2_AddressBook/addressBook/src/main/java/com/jc/addressBook/repository/AddressBookRepository.java
@@ -1,2 +1,21 @@
-package com.jc.addressBook.repository;public interface AddressBookRepository {
+package com.jc.addressBook.repository;
+
+import com.jc.addressBook.model.Entry;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AddressBookRepository extends CrudRepository<Entry, Long> {
+
+    List<Entry> findByEmail(String email);
+    List<Entry> findByFirstName(String firstName);
+    List<Entry> findByLastName(String lastName);
+    List<Entry> findByPhoneNumber(String phoneNumber);
+    void deleteByEmail(String email);
+
 }
+
+
+
